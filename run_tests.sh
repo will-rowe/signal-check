7ab73d80863ea07bf8b08169c69c737ccf922e7d
Will Rowe <w.p.m.rowe@gmail.com>
2019-09-24 17:10:21 +0100

    add check for pipeline output  to travis

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
+resultCheck="results/assembly-unpolished.fasta"
 cmd="nextflow run main.nf --cpus 2 --mem 4GB --reads ./data/ecoli_small.fasta --output results -profile conda --assembler miniasm"
 echo "Starting nextflow..."
 echo $cmd
 echo "-------------------------------------------------------"
 eval $cmd
 echo "-------------------------------------------------------"
+
+if test -f "$resultCheck"; then
+    echo "check for initial assembly passed"
+else
+    echo "check for initial assembly failed"
+    exit -1
+fi
 echo "cleaning up..."
 eval "make"
