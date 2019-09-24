cb912e45214f0a90e6d12dfbb44df537230ced58
Will Rowe <w.p.m.rowe@gmail.com>
2019-09-24 16:37:11 +0100

    moving tests

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 clean:
 	rm -rf work/
 	rm -rf .nextflow.log*
-	cd tests && rm -rf work && rm -rf .nextflow.log* && rm -rf results
+	rm -rf results
--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-git clone https://github.com/will-rowe/signal-check && cd signal-check
-
-# run tests
-run_name="Test run: "$(date +%s)
 cmd="nextflow run main.nf --cpus 2 --mem 4GB --reads ./data/ecoli_small.fasta --output results -profile conda --assembler miniasm"
-echo "Starting nextflow... Command:"
+echo "Starting nextflow..."
 echo $cmd
 echo "-------------------------------------------------------"
 eval $cmd
+echo "-------------------------------------------------------"
+echo "cleaning up..."
+eval "make"
